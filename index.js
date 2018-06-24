ff69cb27d744caff2abcd8b3b6ac964c935ac303
Ivo Nederlof <developer@edept.nl>
2018-06-24 13:09:04 +0200

    Created csv writer, for manual translations

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const async = require('async');
-
-const xlfProcesser = require('./lib/xlf-file-processor');
+const xlfFileProcessor = require('./lib/xlf-file-processor');
 const xlfTranslator = require('./lib/xlf-translator');
 const xml2js = require('xml2js');
 const config = require('./translate.config');
 const constants = require('./lib/constants');
 const errors = require('./lib/errors');
 
-translateAutomaticFile((err) => {
-    console.log('Done ;)');
-});
+start();
+
+function start() {
+
+    console.warn('Initiated xlf-translator ...');
+
+    xlfFileProcessor.doesHaveMessages('./files/messages', (err, exists) => {
+        if (exists) {
+            console.warn('found message files, started indexing manual translations\n');
+            translateFileFromManualCsvDirectory((err) => {
+                if (err) {
+                    console.error(err);
+                }
+                console.log('reindexed your translations')
+            })
+        } else {
+            console.warn('did not found any messages, started translating with google ...\n');
+            translateFileWithGoogleApi((err) => {
+                if (err) {
+                    console.error(err);
+                }
+                console.log('created new translations')
+            });
+        }
+    });
+}
 
 /**
  * Translate a file that has target, else it will break
  * @param callback
  */
-function translateFromFile(callback) {
+function translateFileFromManualCsvDirectory(callback) {
 
     callback();
 }
@@ -24,7 +46,7 @@ function translateFromFile(callback) {
  * Translate a file automaticy from a specific path, this means that it will generate a file that was translated by google
  * @param callback
  */
-function translateAutomaticFile(callback) {
+function translateFileWithGoogleApi(callback) {
 
     async.waterfall([
         // first validate before processing
@@ -43,7 +65,7 @@ function translateAutomaticFile(callback) {
         (callback) => {
 
             // get the file string
-            xlfProcesser.readXlfFile(`./${constants.OUTPUT_DIRECTORY}/${constants.SOURCE_FILE_NAME}.${constants.FILE_TYPE}`, (err, xlfFileAsString) => {
+            xlfFileProcessor.readXlfFile(`./${constants.OUTPUT_DIRECTORY}/${constants.SOURCE_FILE_NAME}.${constants.FILE_TYPE}`, (err, xlfFileAsString) => {
                 callback(err, xlfFileAsString);
             })
         },
@@ -53,18 +75,32 @@ function translateAutomaticFile(callback) {
             const file = xlfFileAsString.xliff.file[0];
             config.fromLanguage = config.fromLanguage ? config.fromLanguage : file.$['source-language'];
             const body = file.body[0]['trans-unit'];
-            const bodies = [];
 
             async.forEach(config.toLanguage, (languageToTranslate, next) => {
 
+                async.waterfall([
+                    (callback) => {
+                        xlfTranslator.translateBody(body, config.fromLanguage, languageToTranslate, (err, newBody) => {
+                            callback(err, newBody);
+                        });
+                    },
+                    (newBody, callback) => {
 
+                        xlfFileProcessor.createCsvFromBody(`./${constants.OUTPUT_DIRECTORY}/${constants.CSV_OUTPUT_DIRECTORY}/${constants.CSV}/translation.${languageToTranslate}.${constants.CSV}`, newBody, (err) => {
+                            callback(err, newBody);
+                        });
+                    },
+                    (newBody, callback) => {
+                        // substitute the body with the new body and parse it back to the xml before saving
+                        xlfFileAsString.xliff.file[0].body[0]['trans-unit'] = newBody;
+                        const builder = new xml2js.Builder();
+                        const xml = builder.buildObject(xlfFileAsString);
 
-                xlfTranslator.translateBody(body, config.fromLanguage, languageToTranslate, (err, newBody) => {
-                    const language = {language: languageToTranslate};
-                    Object.assign(language, newBody);
-                    bodies.push(newBody);
-
- 
+                        xlfFileProcessor.createXlfFile(`./${constants.OUTPUT_DIRECTORY}/${constants.OUTPUT_FILE_NAME}/${constants.OUTPUT_FILE_NAME}.${languageToTranslate}.${constants.FILE_TYPE}`, xml, (err) => {
+                            callback(err);
+                        });
+                    }
+                ], (err) => {
                     // wait otherwise google will block this ip address
                     setTimeout(() => {
                         console.warn(`translated ${languageToTranslate}`);
@@ -72,25 +108,9 @@ function translateAutomaticFile(callback) {
                     }, 3000)
                 });
             }, (err) => {
-                callback(err, xlfFileAsString, bodies);
-            });
-        },
-        (xlfFileAsString, bodies, callback) => {
-
-            async.forEach(bodies, (body, next) => {
-                // substitute the body with the new body and parse it back to the xml before saving
-                xlfFileAsString.xliff.file[0].body[0]['trans-unit'] = body;
-                const builder = new xml2js.Builder();
-                const xml = builder.buildObject(xlfFileAsString);
-
-                xlfProcesser.createXlfFile(`./${constants.OUTPUT_DIRECTORY}/${constants.OUTPUT_FILE_NAME}.${body.language}.${constants.FILE_TYPE}`, xml, () => {
-                    next();
-                })
-
-            }, (err) => {
                 callback(err);
             });
-        }
+        },
     ], (err) => {
         callback(err);
     });
