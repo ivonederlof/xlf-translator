6164e95517c94432bd6cb07bbc7a352fb1094591
Ivo Nederlof <developer@edept.nl>
2019-03-12 17:35:08 +0100

    Asci

--- bin/translate.js
+++ bin/translate.js
@@ -8,17 +8,15 @@ global.appRoot = process.cwd();
 global.translatorConfig = require(`${appRoot}/${constants.CONFIG_NAME}`);
 
 console.log(`
-                                 888                                      888          888                    
-                                 888                                      888          888                    
-                                 888                                      888          888                    
-88888b.   .d88b.  888  888       888888 888d888 8888b.  88888b.  .d8888b  888  8888b.  888888 .d88b.  888d888 
-888 "88b d88P"88b \`Y8bd8P'       888    888P"      "88b 888 "88b 88K      888     "88b 888   d88""88b 888P"   
-888  888 888  888   X88K  888888 888    888    .d888888 888  888 "Y8888b. 888 .d888888 888   888  888 888     
-888  888 Y88b 888 .d8""8b.       Y88b.  888    888  888 888  888      X88 888 888  888 Y88b. Y88..88P 888     
-888  888  "Y88888 888  888        "Y888 888    "Y888888 888  888  88888P' 888 "Y888888  "Y888 "Y88P"  888     
-              888                                                                                             
-         Y8b d88P                                                                                             
-          "Y88P"                                                                                              
+            88    ad88                                                         88                                         
+            88   d8"         ,d                                                88              ,d                         
+            88   88          88                                                88              88                         
+8b,     ,d8 88 MM88MMM     MM88MMM 8b,dPPYba, ,adPPYYba, 8b,dPPYba,  ,adPPYba, 88 ,adPPYYba, MM88MMM ,adPPYba,  8b,dPPYba,
+ \`Y8, ,8P'  88   88 aaaaaaaa 88    88P'   "Y8 ""     \`Y8 88P'   \`"8a I8[    "" 88 ""     \`Y8   88   a8"     "8a 88P'   "Y8
+   )888(    88   88 """""""" 88    88         ,adPPPPP88 88       88  \`"Y8ba,  88 ,adPPPPP88   88   8b       d8 88        
+ ,d8" "8b,  88   88          88,   88         88,    ,88 88       88 aa    ]8I 88 88,    ,88   88,  "8a,   ,a8" 88        
+8P'     \`Y8 88   88          "Y888 88         \`"8bbdP"Y8 88       88 \`"YbbdP"' 88 \`"8bbdP"Y8   "Y888 \`"YbbdP"'  88        
+                                                                                                                                                                                                                   
 
 `);
 
